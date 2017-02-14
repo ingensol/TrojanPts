71d8f9374b2ca375ea42bd45e22c95a9b25f922c
Juan Chavez Jr <juan@dexteroustechnology.com>
2017-02-14 14:20:56 -0800

    create context to aquire token

--- www/js/index.js
+++ www/js/index.js
@@ -86,9 +86,13 @@ var app = {
         }, app.error);
     },
     acquireToken: function () {
+
         if (app.authContext == null) {
             app.error('Authentication context isn\'t created yet. Create context first');
             return;
+        }
+        else {
+            app.createContext;
         }
 
         app.authContext.acquireTokenAsync(resourceUrl, appId, redirectUrl)

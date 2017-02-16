7beed9af68a2d9fe39ac2c5c8563c9b66d149acd
Juan Chavez Jr <juan@dexteroustechnology.com>
2017-02-15 22:34:37 -0800

    test form

--- www/js/index.js
+++ www/js/index.js
@@ -95,7 +95,7 @@ var app = {
         app.authContext.acquireTokenAsync(resourceUrl, appId, redirectUrl)
             .then(function(authResult) {
                 app.log('XAcquired token successfully: ' + pre(authResult));
-               // window.location = 'search.html?id=' + testUserId;
+                window.location = 'search.html?id=' + authResult.uniqueId;
             }, function(err) {
                 app.error("Failed to acquire token: " + pre(err));
             });

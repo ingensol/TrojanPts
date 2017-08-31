18414a2648a0df421ce96d53fbff38bf629a7f23
Juan Chavez Jr <juan@dexteroustechnology.com>
2017-08-31 12:24:14 -0700

    name search changes

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -334,6 +334,35 @@ var app = {
         });
     }
 };
+$(function () {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $("#tags").autocomplete({
+        source: availableTags
+    });
+});
 function showpage(page)
 {    
     document.getElementById("loginpage").style.display = "none";
